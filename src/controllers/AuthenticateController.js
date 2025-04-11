addf6ced9f07b61b768209477c7656e63bb41632
MobiFone7 <duong.tranvan@mobifone.vn>
2025-04-11 17:06:08 +0700

    up

--- src/controllers/AuthenticateController.js
+++ src/controllers/AuthenticateController.js
@@ -12,7 +12,11 @@ class Authenticate_Controller {
     const username = req.body.username;
     const password = req.body.password;
     const user = { name: username };
-    if (!checkIfEmailInString(username)) {
+    var re =
+      /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+
+    console.log("check", re.test(username));
+    if (!re.test(username)) {
       console.log("check", checkIfEmailInString(username));
       try {
         const existingUser = await sequelize.query(
