b9a6f581c2d8ad761c049ce8adbe803e3baad704
MobiFone7 <duong.tranvan@mobifone.vn>
2025-04-11 16:54:26 +0700

    up

--- src/controllers/AuthenticateController.js
+++ src/controllers/AuthenticateController.js
@@ -3,13 +3,43 @@ const jwt = require("jsonwebtoken");
 const ldap = require("ldapjs");
 const DbConnection = require("../../DbConnection");
 const { sequelize } = require('../models'); // Import s
-
+const checkIfEmailInString = require("../utils/helper");
+const customEncode = require("../utils/helper");
 class Authenticate_Controller {
+
+
   async index(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const user = { name: username };
-    if (username && password) {
+    if (!checkIfEmailInString(username)) {
+      try {
+        const existingUser = await sequelize.query(
+          `SELECT * FROM db01_owner.v_c7_admin_user 
+          user_name = :username
+         `,
+          {
+            replacements: { username: username },
+            type: sequelize.QueryTypes.SELECT,
+          });
+        if (existingUser.password) {
+          if (customEncode(password) == existingUser.password) {
+            res.json({
+              accessToken: accessToken,
+              username: username,
+              roles: []
+            });
+          } else {
+            res.status(401).json({ error: "sai mat khau" }); // This runs as well.
+
+          }
+        }
+      } catch (e) {
+        console.log("error", error)
+        res.status(401).json({ error: "có lỗi xảy ra" }); // This runs as well.
+      }
+
+    } else if (username && password) {
       var client = ldap.createClient({
         url: process.env.LDAP_URI,
         timeout: 5000,
@@ -25,44 +55,10 @@ class Authenticate_Controller {
             expiresIn: "10d",
           });
           try {
-            const existingUserEmail = await sequelize.query(
-              `SELECT * FROM web_user where 
-              user_email = :username
-             `,
-              {
-                replacements: { username: username },
-                type: sequelize.QueryTypes.SELECT,
-              });
-            if (existingUserEmail.length == 0) {
-              const userNamePrefix = username.split("@")[0];
-              await sequelize.query(
-                `insert into web_user (user_email, user_name, status) 
-                     values(
-                    :username,
-                    :userNamePrefix,
-                    1
-                     ) 
-                     `,
-                {
-                  replacements: { username: username, userNamePrefix: userNamePrefix },
-                  type: sequelize.QueryTypes.INSERT,
-                });
-            }
-
-
-
-            const queryRoles = await sequelize.query(
-              `select a.*, b.* from web_user_email a 
-            left join web_user b on a.user_email =  b.user_email  where a.user_email =:username `,
-              {
-                replacements: { username: username },
-                type: sequelize.QueryTypes.SELECT,
-              });
-
             res.json({
               accessToken: accessToken,
               username: username,
-              roles: queryRoles
+              roles: []
             });
           } catch (error) {
             console.log("error", error)
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 function lowerCaseKeys(obj) {
   return Object.keys(obj).reduce((accumulator, key) => {
     accumulator[key.toLowerCase()] = obj[key];
@@ -18,5 +20,27 @@ function checkFileType(file, cb) {
     cb("Error: Images Only!");
   }
 }
+function checkIfEmailInString(text) {
+  var re =
+    /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+  return re.test(text);
+}
+
+function customEncode(input) {
+  // 1. Băm SHA1, xuất ra dưới dạng hex
+  const sha1HashHex = crypto.createHash('sha1').update(input).digest('hex');
+
+  // 2. Tách mỗi 2 ký tự (hex) -> byte -> push vào Buffer
+  const byteArray = [];
+  for (let i = 0; i < sha1HashHex.length; i += 2) {
+    byteArray.push(parseInt(sha1HashHex.substr(i, 2), 16));
+  }
+
+  // 3. Chuyển về Buffer và encode base64
+  const result = Buffer.from(byteArray).toString('base64');
+  return result;
+}
+module.exports = customEncode;
+module.exports = checkIfEmailInString;
 module.exports = lowerCaseKeys;
 module.exports = checkFileType;
