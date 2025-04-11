c342ed604258ddd2d9c19dd52fae0d24087150f0
MobiFone7 <duong.tranvan@mobifone.vn>
2025-04-11 16:59:07 +0700

    up

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,20 +6,7 @@ function lowerCaseKeys(obj) {
     return accumulator;
   }, {});
 }
-function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toString());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
-  }
-}
 function checkIfEmailInString(text) {
   var re =
     /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
@@ -43,4 +30,3 @@ function customEncode(input) {
 module.exports = customEncode;
 module.exports = checkIfEmailInString;
 module.exports = lowerCaseKeys;
-module.exports = checkFileType;
