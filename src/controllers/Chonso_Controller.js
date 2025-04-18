cd2e52dec87d357eb560f90b31e5576564090a09
MobiFone7 <duong.tranvan@mobifone.vn>
2025-04-18 08:34:36 +0700

    up

--- src/controllers/Chonso_Controller.js
+++ src/controllers/Chonso_Controller.js
@@ -79,7 +79,7 @@ class ChonsoController {
       //   `;
 
       let sql = `
-        ELECT v1.tel_number, v1.SPE_NUMBER_TYPE, v1.loai_ck, v1.SHOP_CODE, v1.CHANGE_DATETIME, nvl(v2.is_hold,v1.is_hold) is_hold FROM (  
+        SELECT v1.tel_number, v1.SPE_NUMBER_TYPE, v1.loai_ck, v1.SHOP_CODE, v1.CHANGE_DATETIME, nvl(v2.is_hold,v1.is_hold) is_hold FROM (  
         SELECT a.TEL_NUMBER, a.SPE_NUMBER_TYPE,
           DECODE(spe_number_type, '1', 'CK1500', '2', 'CK1200', '3', 'CK1000', '4', 'CK800',
          '5', 'CK500', '6', 'CK400', '7', 'CK300', '8', 'CK250', '9', 'CK150', '10', 'Tự do', 'KXD') loai_ck, a.SHOP_CODE, a.CHANGE_DATETIME, '0' is_hold
