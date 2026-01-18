2c91004103f22b7eb6aa9fd76ca72326d2e8f269
MobiFone7 <duong.tranvan@mobifone.vn>
2026-01-18 23:00:37 +0700

    up

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ limit: "5mb", extended: true }));
-app.use(bodyParser.json({ limit: "5mb" }));
-app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 console.log(
   "check", process.env.DB, process.env.USER_WEBSITE, process.env.PASSWORD_WEBSITE
 )
